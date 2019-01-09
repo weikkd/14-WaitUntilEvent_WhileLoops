2900bac8ee83654ebb63bee9140667f2d5c7ae11
weikkd <weikkd@rose-hulman.edu>
2019-01-09 10:29:07 -0500

    Done.

--- src/m1r_console_input_examples.py
+++ src/m1r_console_input_examples.py
@@ -6,25 +6,31 @@ This module demonstrates how to INPUT from the CONSOLE:
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Kaitlin Weik.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ###############################################################################
-# TODO: 2.  Read and run this program.  Then do the following problems,
+# DONE: 2.  Read and run this program.  Then do the following problems,
 #   putting your answers RIGHT HERE IN THIS DOCUMENT.
 #
 #   1. Write a line of code that would input an INTEGER from the
 #      console, storing the integer in a variable called 'x'.
 #      Write your line here:
-#
+
+x = int(input('Please enter an integer:'))
+
 #   2. Write a line of code that would input an FLOAT from the console,
 #      storing the float in a variable called 'x'.
 #      Write your line here:
-#
+
+x = float(input('Please enter a float:'))
+
 #   3. Write a line of code that would input an STRING from the console,
 #      storing the string in a variable called 'x'.
 #      Write your line here:
-#
+
+x = str(input('Please enter a string:'))
+
 #   4. What happens if you (the user) enter something OTHER than a
 #      single integer (e.g., you enter
 #        five
@@ -41,6 +47,8 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
 #     a. Find someone who has had HER answer checked.
 #        Ask her to check YOUR answers to the above.
 #     b. Change the above TO DO to DONE.
+#
+#       YOU GET A VALUE ERROR
 #
 #   As always, ask questions as needed!
 ###############################################################################
